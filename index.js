194e49ade005bd8db2d77fc5e0a4ce3c5f649426
Aires-Leo <leonardo_aires@live.com>
2023-09-05 21:54:02 -0300

    Aula sobre laços

--- index.js
+++ index.js
@@ -1,5 +1,86 @@
+/*
 let soma = prompt("Digite um número");
-while (soma < 100) {
-    console.log("Total 70")
+while (soma < 10) {
+    console.log(soma)
     soma ++
 }
+
+let estomago = 0;
+
+while (estomago < 100) {
+    console.log("Quero comer mais coxinhas")
+    estomago = estomago + 10;
+}
+*/
+//código acima não funcionou
+
+
+/*
+let numeros = [ ];
+
+const chamarPrompt = ( ) => {
+    const numero = Number(prompt("Digite um número"));
+    numeros.push(numero);
+}
+
+while (!numeros.includes(0)) {
+    chamarPrompt();
+}
+    if (numeros.includes(0)) {
+    let i = 0;
+    let soma = 0;
+    while (i < numeros.length -1) {
+        soma = soma + numeros[1];
+        i++;
+    }
+    console.log(soma);
+}
+*/
+//Exercício 1 Slides
+/*
+for (let i = 0; i<10; i++) {
+    console.log(i)
+}
+
+const numeros = [14, 67, 89, 15, 23]
+for(let i = 0; i<5; i++) {
+    const elemento = numeros[i]
+    console.log(elemento)
+}
+*/
+
+//Ecercício 2 Slides
+/*
+const numeros = [11, 15, 18, 14, 12, 13]
+
+function devolveMaiorNumero(array) {
+
+    let maiorNumero = 0
+
+    for(let i = 0; i < array.length; i++) {
+        let numeroAtual = array[i]
+
+        if(numeroAtual >= maiorNumero) {
+            maiorNumero = numeroAtual
+        }
+    }
+    console.log(`O maior número do array é ${maiorNumero}`)
+}
+devolveMaiorNumero(numeros)
+*/
+//Exercício 3 Slides
+/*
+let palavras = ["Oi", "sumida", "tudo", "bem?", "Saudades"]
+let frase = "" 
+for (palavra of palavras) {
+    frase += palavra + ' '
+         
+}
+console.log(frase)
+*/
+
+
+
+
+//Exercícios de Interpretação de códigos
+//1)
